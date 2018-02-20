071bceeba0b90ba3c16d0e73740e635b891dc4c4
toxa16 <toxa16@gmail.com>
2018-02-20 18:52:10 +0200

    cors: explicit allow-methods & allow-headers

--- src/cors.ts
+++ src/cors.ts
@@ -1,7 +1,15 @@
 import { ServerResponse } from 'http';
+import { HttpMethod } from '@stayer/interfaces';
+
+/*const headers = [
+  'Accept',
+  'Accept-Encoding',
+  'Accept-Language',
+  'Authorization',
+];*/
 
 export default function cors(res: ServerResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', Object.values(HttpMethod).join(', '));
 }
